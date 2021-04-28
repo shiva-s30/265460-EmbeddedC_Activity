0fcfe523ec51e861b0000fb1d0ec6c41f2159428
shiva-s30 <65439506+shiva-s30@users.noreply.github.com>
2021-04-28 20:19:26 +0530

    Update activity_main.c

--- activity_main.c
+++ activity_main.c
@@ -14,16 +14,23 @@
  * Include files
  */
 #include "led_state.h"
+#include "adc_read.h"
 #include "main.h"
 
 void port_init(void)
 {
     DDRB &= ~(_BV(button_sensor));
     DDRB &= ~(_BV(heater));
+    DDRB |= _BV(led_out);
+    PORTB |= _BV(led_out);
 }
 
 int main(void)
 {
+    adc_init();
+    uint16_t temp_value = 0;
+
+
     while(1)
     {
             if((PINB & set_state(button_sensor) >> button_sensor))
@@ -31,6 +38,7 @@ int main(void)
                     if((PINB & set_state(heater) >> heater))
                         {
                             led_state(LED_ON);
+                            temp_value = adc_read(0);
                         }
             }
             else
