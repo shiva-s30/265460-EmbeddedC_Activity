963767c10ec2cee9fb9af6c8612949632158a7de
shiva-s30 <65439506+shiva-s30@users.noreply.github.com>
2021-04-30 00:36:13 +0530

    Update adc_read.h

--- inc/adc_read.h
+++ inc/adc_read.h
@@ -1,6 +1,17 @@
 #ifndef ADC_READ_H_
 #define ADC_READ_H_
 
+/**
+ * @file adc_read.h
+ * @author Shiva S
+ * @brief required header files and macro definitions
+ * @version 0.1
+ * @date 2021-04-27
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
  /**
  * Include files
  */
