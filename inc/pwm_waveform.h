574fcfb64fbc9858355d793b67f6ccd806868519
shiva-s30 <65439506+shiva-s30@users.noreply.github.com>
2021-04-30 00:36:34 +0530

    Update pwm_waveform.h

--- inc/pwm_waveform.h
+++ inc/pwm_waveform.h
@@ -1,6 +1,17 @@
 #ifndef PWM_WAVEFORM_H_
 #define PWM_WAVEFORM_H_
 
+/**
+ * @file pwm_waveform.h
+ * @author Shiva S
+ * @brief required header files and macro definitions
+ * @version 0.1
+ * @date 2021-04-27
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
  /**
  * Include files
  */
@@ -8,6 +19,6 @@
 #include "main.h"
 
 void pwm_init();
-void pwm_waveform(uint16_t adc_value);
+char pwm_waveform(uint16_t adc_value);
 
 #endif
