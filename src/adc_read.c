4cf1854a903684965f27b7b0a6a4f6e4f79bc6a1
shiva-s30 <65439506+shiva-s30@users.noreply.github.com>
2021-04-30 19:18:30 +0530

    Update adc_read.c

--- src/adc_read.c
+++ src/adc_read.c
@@ -17,23 +17,32 @@
 #include <avr/io.h>
 #include "adc_read.h"
 
+/**
+* Peripheral Initialization
+*/
 void adc_init()
 {
-    ADMUX=(1<<REFS0);
-    ADCSRA=(1<<ADEN)|(7<<ADPS0);
+    ADMUX = (1<<REFS0);
+    ADCSRA = (1<<ADEN)|(7<<ADPS0);
 }
 
+ /**
+ * @brief Function to read value of ADC
+ *
+ * @param[in] ch
+ *
+ */
 uint16_t adc_read(uint8_t ch)
 {
-    //Select ADC Channel ch must be 0-7
-    ADMUX&=0xf8;
-    ch=ch&0b00000111;
-    ADMUX|=ch;
-    //Start single conversion
-    ADCSRA|=(1<<ADSC);
-    //Wait for conversion to complete
-    while(!(ADCSRA & (1<<ADIF)));
-    //Clear ADIF by writing one to it
-    ADCSRA|=(1<<ADIF);
+
+    ADMUX &= 0xf8; //Selecting ADC channel in the range of 0 to 7
+    ch = ch & 0b00000111;
+    ADMUX |= ch;
+
+    ADCSRA |= (1 << ADSC); //Starting Single Conversion
+
+    while(!(ADCSRA & (1 << ADIF))); //Condition to check if conversion has completed
+
+    ADCSRA |= (1 << ADIF); //Clearing the status of ADIF
     return(ADC);
 }
